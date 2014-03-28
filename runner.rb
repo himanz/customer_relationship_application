97ae5dd4be9d1fa8087e07b8e494b9c9cd6b259c
Jonathan Chang <jonoschang7@gmail.com>
2014-03-28 12:53:45 -0400

    complete wrong input function

--- runner.rb
+++ runner.rb
@@ -3,6 +3,21 @@ class Runner
 		puts "World of CRM"
 		puts "1. Add a contact"
 	end
+  
+  def wrong_input
+  	puts "Wrong input, enter again"
+  end
+
+	def run
+		main_menu
+		input = gets.chomp.to_i
+		if input == 1
+			add_contact
+		else
+			wrong_input
+		end
+	end
 end
 
 running = Runner.new
+running.run
